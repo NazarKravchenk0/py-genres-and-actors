f17587a72f8d8df1b49c41b346024607e160eaef
nazarkravchenko <nazarnazarik5@gmail.com>
2026-02-03 16:14:03 +0200

    check1

---
+++ db/migrations/0001_initial.py
@@ -0,0 +1,41 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies: list[tuple[str, str]] = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Genre",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="Actor",
+            fields=[
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("first_name", models.CharField(max_length=255)),
+                ("last_name", models.CharField(max_length=255)),
+            ],
+        ),
+    ]
