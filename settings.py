d2d0f3f38ce8d359b004ab1b79ca7a36c93731fb
nazarkravchenko <nazarnazarik5@gmail.com>
2026-02-03 16:09:45 +0200

    check

--- settings.py
+++ settings.py
@@ -1,12 +1,43 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
+DEBUG = True
+
+ALLOWED_HOSTS: list[str] = []
+
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    "db",
+)
+
+MIDDLEWARE = (
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+)
+
+ROOT_URLCONF = "urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+            ],
+        },
+    }
+]
 
 DATABASES = {
     "default": {
@@ -16,5 +47,3 @@ DATABASES = {
 }
 
 USE_TZ = False
-
-INSTALLED_APPS = ("db",)
